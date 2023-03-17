95207cdd0be57a94fc0df4089b4d8ea9f32a600b
@felipe-loose <felipe.loose@gmail.com>
2023-03-17 14:43:49 +0100

    added ClaAdcFir Example (backup).

---
+++ Example_2806xClaAdcFir/Debug/Example_2806xClaAdcFir.d
@@ -0,0 +1,129 @@
+# FIXED
+
+Example_2806xClaAdcFir.obj: ../Example_2806xClaAdcFir.c
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/DSP28x_Project.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_Cla_typedefs.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Device.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Adc.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_BootVars.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Cla.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Comp.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_CpuTimers.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_DevEmu.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Dma.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_ECan.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_ECap.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_EPwm.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_EQep.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Gpio.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_HRCap.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_I2c.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Mcbsp.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_NmiIntrupt.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_PieCtrl.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_PieVect.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Spi.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Sci.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_SysCtrl.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Usb.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_XIntrupt.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_Examples.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_GlobalPrototypes.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_EPwm_defines.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_I2c_defines.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_Dma_defines.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_Cla_defines.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_DefaultISR.h
+Example_2806xClaAdcFir.obj: ../CLAShared.h
+Example_2806xClaAdcFir.obj: C:/ti/c2000/C2000Ware_4_01_00_00/libraries/math/IQmath/c28/include/IQmathLib.h
+Example_2806xClaAdcFir.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/limits.h
+Example_2806xClaAdcFir.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/_ti_config.h
+Example_2806xClaAdcFir.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/linkage.h
+Example_2806xClaAdcFir.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/string.h
+Example_2806xClaAdcFir.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/cdefs.h
+Example_2806xClaAdcFir.obj: C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/xlocale/_string.h
+
+../Example_2806xClaAdcFir.c:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/DSP28x_Project.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_Cla_typedefs.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Device.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Adc.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_BootVars.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Cla.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Comp.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_CpuTimers.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_DevEmu.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Dma.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_ECan.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_ECap.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_EPwm.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_EQep.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Gpio.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_HRCap.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_I2c.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Mcbsp.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_NmiIntrupt.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_PieCtrl.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_PieVect.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Spi.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Sci.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_SysCtrl.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_Usb.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/include/F2806x_XIntrupt.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_Examples.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_GlobalPrototypes.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_EPwm_defines.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_I2c_defines.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_Dma_defines.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_Cla_defines.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/include/F2806x_DefaultISR.h:
+
+../CLAShared.h:
+
+C:/ti/c2000/C2000Ware_4_01_00_00/libraries/math/IQmath/c28/include/IQmathLib.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/limits.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/_ti_config.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/linkage.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/string.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1210/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/xlocale/_string.h:
+
---
+++ Example_2806xClaAdcFir/Debug/subdir_vars.mk
@@ -0,0 +1,98 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/cmd/28069_RAM_CLA_lnk.cmd \
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/cmd/F2806x_Headers_nonBIOS.cmd 
+
+ASM_SRCS += \
+../CLA_FIR.asm \
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_CodeStartBranch.asm \
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_usDelay.asm 
+
+C_SRCS += \
+../Example_2806xClaAdcFir.c \
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_Adc.c \
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_DefaultIsr.c \
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/source/F2806x_GlobalVariableDefs.c \
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_Gpio.c \
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_PieCtrl.c \
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_PieVect.c \
+C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_SysCtrl.c 
+
+C_DEPS += \
+./Example_2806xClaAdcFir.d \
+./F2806x_Adc.d \
+./F2806x_DefaultIsr.d \
+./F2806x_GlobalVariableDefs.d \
+./F2806x_Gpio.d \
+./F2806x_PieCtrl.d \
+./F2806x_PieVect.d \
+./F2806x_SysCtrl.d 
+
+OBJS += \
+./CLA_FIR.obj \
+./Example_2806xClaAdcFir.obj \
+./F2806x_Adc.obj \
+./F2806x_CodeStartBranch.obj \
+./F2806x_DefaultIsr.obj \
+./F2806x_GlobalVariableDefs.obj \
+./F2806x_Gpio.obj \
+./F2806x_PieCtrl.obj \
+./F2806x_PieVect.obj \
+./F2806x_SysCtrl.obj \
+./F2806x_usDelay.obj 
+
+ASM_DEPS += \
+./CLA_FIR.d \
+./F2806x_CodeStartBranch.d \
+./F2806x_usDelay.d 
+
+OBJS__QUOTED += \
+"CLA_FIR.obj" \
+"Example_2806xClaAdcFir.obj" \
+"F2806x_Adc.obj" \
+"F2806x_CodeStartBranch.obj" \
+"F2806x_DefaultIsr.obj" \
+"F2806x_GlobalVariableDefs.obj" \
+"F2806x_Gpio.obj" \
+"F2806x_PieCtrl.obj" \
+"F2806x_PieVect.obj" \
+"F2806x_SysCtrl.obj" \
+"F2806x_usDelay.obj" 
+
+C_DEPS__QUOTED += \
+"Example_2806xClaAdcFir.d" \
+"F2806x_Adc.d" \
+"F2806x_DefaultIsr.d" \
+"F2806x_GlobalVariableDefs.d" \
+"F2806x_Gpio.d" \
+"F2806x_PieCtrl.d" \
+"F2806x_PieVect.d" \
+"F2806x_SysCtrl.d" 
+
+ASM_DEPS__QUOTED += \
+"CLA_FIR.d" \
+"F2806x_CodeStartBranch.d" \
+"F2806x_usDelay.d" 
+
+ASM_SRCS__QUOTED += \
+"../CLA_FIR.asm" \
+"C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_CodeStartBranch.asm" \
+"C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_usDelay.asm" 
+
+C_SRCS__QUOTED += \
+"../Example_2806xClaAdcFir.c" \
+"C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_Adc.c" \
+"C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_DefaultIsr.c" \
+"C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/headers/source/F2806x_GlobalVariableDefs.c" \
+"C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_Gpio.c" \
+"C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_PieCtrl.c" \
+"C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_PieVect.c" \
+"C:/ti/c2000/C2000Ware_4_01_00_00/device_support/f2806x/common/source/F2806x_SysCtrl.c" 
+
+
